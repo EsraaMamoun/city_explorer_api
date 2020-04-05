ab69e9844aea221caac07e62c2de0a8fc749cd3c
EsraaMamoun <esraamamoun5@gmail.com>
2020-04-05 20:15:24 +0300

    added weather

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors());
@@ -13,7 +13,7 @@ app.get('/', (request, response) => {
     response.status(200).send('The Home Page.');
 });
 
-console.log('PORT:',PORT);
+console.log('PORT:', PORT);
 
 // console.log('cors:',cors);
 // console.log('app:',app);
@@ -22,19 +22,21 @@ app.get('/bad', (request, response) => {
     throw new Error('ERROR!');
 });
 
+////////////////////////////////Location///////////////////////////////////////////
 
-function Location(city,geoData) {
+function Location(city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData[0].display_name;
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
 }
-app.get('/location',(request,response) => {
+app.get('/location', (request, response) => {
     try {
         const geoData = require('./data/geo.json');
         const city = request.query.city;
-        const locationData = new Location(city,geoData);
+        const locationData = new Location(city, geoData);
         response.status(200).json(locationData);
+        console.log(city);
     } catch (error) {
         errorHandler(error, request, response);
     }
@@ -45,9 +47,30 @@ function errorHandler(error, request, response) {
 }
 
 
-function notFound(request, response) {
-    response.status(404).send('NOT FOUND!');
+
+///////////////////////////////////////////Weather///////////////////////////////////////////
+
+function Weather(forecast, darkskyData) {
+    this.forecast = forecast;
+    this.time = darkskyData.data[0].valid_date;
 }
+app.get('/weather', (request, response) => {
+    try {
+        const darkskyData = require('./data/darksky.json');
+        const forecast = request.query.forecast;
+        const weatherData = new Weather(forecast, darkskyData);
+        response.status(200).json(weatherData);
+        console.log(forecast);
+    } catch (error) {
+        errorHandler(error, request, response);
+    }
+});
+
+///////////////////////////////////////////Not Gound/////////////////////////////////////////
+
+    function notFound(request, response) {
+        response.status(404).send('NOT FOUND!');
+    }
 app.use('*', notFound);
 
 app.listen(PORT, () => console.log(`The server is up and running on ${PORT}`));
